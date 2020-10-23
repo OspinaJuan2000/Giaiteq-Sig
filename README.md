a4787af766009cb0132c7462a80cd87a4e3943cb
Juan Ospina <ospinajuan2000@gmail.com>
2020-10-22 20:28:22 -0500

    Config heroku

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Proyecto GIAITEQ (GRUPO DE INVESTIGACIÓN APLICADA A LA PRODUCCIÓN INDUSTRIAL, TEXTIL Y QUÍMICA)
+
+### Probar proyecto: [Giaiteq](https://giaiteq.herokuapp.com/)
--- modelo/conexion.php
+++ modelo/conexion.php
@@ -7,10 +7,10 @@ class Conexion
     */
     public function establecer_conexion()
     {
-        $usuario = "root";
-        $clave = "";
-        $host = "localhost";
-        $bd = "giaiteq";
+        $usuario = "b9b0b286d7afd0";
+        $clave = "4be836b3";
+        $host = "us-cdbr-east-02.cleardb.com";
+        $bd = "heroku_3a4f9a42dbee8cf";
         $conexion = new PDO("mysql:host=$host;dbname=$bd;charset=utf8", $usuario, $clave);
         $conexion->exec("set names utf8");
         
